930217d82a3b8774872d4dab35b576dd8397d1b4
gean <souzaz_eg@outlook.com>
2021-08-27 14:54:15 -0300

    triangulo

---
+++ git/CalculoDoIMC.java
@@ -0,0 +1,11 @@
+//A partir do peso e altura de uma pessoa, calcule o IMC (Índice de massa corpóreo) indicando
+//o estado dessa pessoa baseado na tabela
+class CalculoDoIMC{
+  public static void main (Strimg [] args){
+    double peso=46;
+    double altura=1.57;
+    double alturaAoQuadrado= Math.pow(altura,2);
+    double CalculoDoIMC=peso/alturaAoQuadrado;
+
+  }
+}
